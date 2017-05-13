9425deeacdac69bef58afad450807617de1a5c02
ChengAndy <chengandy@ChengAndydeMacBook-Pro.local>
2017-05-13 09:47:52 +0800

    add scatter plot for Gaussian_NB_example_1.py

--- Gaussian_NB_example_1.py
+++ Gaussian_NB_example_1.py
@@ -1,11 +1,24 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.naive_bayes import GaussianNB
 
-X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])    # features
-Y = np.array([1, 1, 1, 2, 2, 2])    # labels
+# known data
+feature_lst = [[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]]
+X = np.array(feature_lst)    # features
+label_lst = [1, 1, 1, 2, 2, 2]
+Y = np.array(label_lst)    # labels
+print(len(Y), 'data points')
 
 # classifier
 clf = GaussianNB()
 clf.fit(X, Y)
 GaussianNB(priors=None)
-print(clf.predict([[-0.8, -1]]))
+
+# test data
+test_point = [[-0.8, -1]]
+print(clf.predict(test_point))
+
+# scatter plot
+x, y = zip(*feature_lst)
+plt.scatter(x, y)
+plt.show()
